30426b0bd5c248d6395fb7eebf2458b953415b62
Franco Ojeda <fojedavideos@gmail.com>
2023-01-05 17:36:01 -0500

    centrado de elementos, responsive, implementacion de ngFor

--- src/app/secciones/body/body.component.ts
+++ src/app/secciones/body/body.component.ts
@@ -8,9 +8,69 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class BodyComponent implements OnInit {
 
-  //variable para ngSwitch
+  //variable para uso del ngSwitch
   area: string = "";
 
+  //array de habilidades para uso del *ngFor
+  skillsVisual = [{
+    habilidad:"Cámara",
+    descripcion:"Manejo de cámaras DSLR"
+  },
+  {
+    habilidad:"Premiere Pro",
+    descripcion:"7 años de experiencia"
+  },
+  {
+    habilidad:"After Effects",
+    descripcion:"Efectos motion graphic"
+  },
+  {
+    habilidad:"Photoshop",
+    descripcion:"Básico"
+  },
+  {
+    habilidad:"Illustrator",
+    descripcion:"Básico"
+  }
+  ];
+
+  skillsArqui = [{
+    habilidad:"AutoCad",
+    descripcion:"Desarrollo de planos"
+  },
+  {
+    habilidad:"Revit",
+    descripcion:"Basico"
+  },
+  {
+    habilidad:"3D Max",
+    descripcion:"Basico"
+  }
+  ];
+
+  skillsProgram = [{
+    habilidad:"HTML & CSS",
+    descripcion:"Basico"
+  },
+  {
+    habilidad:"JavaScript",
+    descripcion:"Basico"
+  },
+  {
+    habilidad:"Angular",
+    descripcion:"Basico"
+  },
+  {
+    habilidad:"MongoDB",
+    descripcion:"Iniciando"
+  },
+  {
+    habilidad:"XAMPP",
+    descripcion:"Iniciando"
+  }
+  ];
+  
+  // para validacion de formulario
   public formLogin!: FormGroup;
 
   constructor(private formBuilder:FormBuilder) { }
@@ -20,7 +80,7 @@ export class BodyComponent implements OnInit {
       nombre:['', 
         [
           Validators.required,
-          Validators.minLength(10)
+          Validators.minLength(6)
         ]
       ],
       email:['', 
